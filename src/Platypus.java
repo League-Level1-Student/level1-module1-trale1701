7627d65e15e321c5089099f8dfd7739d10bbcf96
598659 <598659@0338S-S-R0B6H0L.sdcs.local>
2019-03-19 14:26:19 -0700

    Platipus, RaceCarRunner ip

---
+++ src/Platypus.java
@@ -0,0 +1,11 @@
+
+public class Platypus {
+      private String name;
+      Platypus(String name){
+    	  this.name = name;
+    	  System.out.println("new platypus created");
+      }
+      	void sayHi(){
+            	System.out.println("The platypus " + name + " is smarter than your average platypus.");
+      	}
+}
--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,16 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar rC = new RaceCar("ford", 5);
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println(rC.getPositionInRace()+"");
 		// 3. Crash the RaceCar
-		
+		rC.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		if(rC.isDamaged()) {rC.pit();}
 		// 5. Help the car move into first place.
-
+		for (int i = 0; i < rC.getPositionInRace(); i++) {
+			rC.overtake();
+		}
 	}
 }
